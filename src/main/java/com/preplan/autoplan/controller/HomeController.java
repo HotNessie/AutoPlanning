cb7072e30747f71c179f426467db93ea1240ec4b
HotNessie <fkdhswpsk7@gmail.com>
2025-01-13 18:36:38 +0900

    feat: "createPlan 기본"

--- src/main/java/com/preplan/autoplan/controller/HomeController.java
+++ src/main/java/com/preplan/autoplan/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.preplan.autoplan.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,9 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model) {
+//        model.addAllAttributes("location",);
         return "home";
     }
-
-
 }
