df7de6e299e5d03e4fabc04b368217f2e0d4f6b6
HotNessie <fkdhswpsk7@gmail.com>
2025-01-20 02:02:55 +0900

    feat: "createPlan 네이버 지도 붙이기만"

--- src/main/java/com/preplan/autoplan/controller/PlanController.java
+++ src/main/java/com/preplan/autoplan/controller/PlanController.java
@@ -1,18 +1,25 @@
 package com.preplan.autoplan.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class PlanController {
 
+    @Value("${ncp.client.id}")
+    private String ncpClientId;
+
     @GetMapping("/plan")
-    public String plan() {
+    public String plan(Model model) {
+        model.addAttribute("ncpClientId", ncpClientId);
         return "createPlan";
     }
 
+    //메뉴 선택시 fragment 호출
     @GetMapping("/hotContent")
     public String getHotContent() {
         return "fragments/planContent :: hotContent";
