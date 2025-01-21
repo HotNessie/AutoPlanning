083aba499734ec168ec713d78cbdd55a6ff53b53
HotNessie <fkdhswpsk7@gmail.com>
2025-01-21 19:40:13 +0900

    feat: Naver search Api Ver 0.1

---
+++ src/main/java/com/preplan/autoplan/Naver/NaverSearchItem.java
@@ -0,0 +1,18 @@
+package com.preplan.autoplan.Naver;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NaverSearchItem {
+    private String title;
+    private String link;
+    private String category;
+    private String description;
+    private String telephone;
+    private String address;
+    private String roadAddress;
+    private String mapx;
+    private String mapy;
+}
