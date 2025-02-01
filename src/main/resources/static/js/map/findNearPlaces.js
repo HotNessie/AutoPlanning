3f5cbf0b7b12c6134a7da07eec88b78bff2f5e58
HotNessie <fkdhswpsk7@gmail.com>
2025-02-02 03:13:10 +0900

    feat: 주변 음식점 search ver 0.1

--- src/main/resources/static/js/map/findNearPlaces.js
+++ src/main/resources/static/js/map/findNearPlaces.js
@@ -2,27 +2,30 @@
 //주변 장소 검색 with 현재위치
 //주변 장소 검색 with 현재위치
 //주변 장소 검색 with 현재위치
-export async function nearbySearch(map,) {
+import { marker } from './marker.js';
+
+export async function nearbySearch(map, infoWindow) {
+
     //@ts-ignore
     const { Place, SearchNearbyRankPreference } = await google.maps.importLibrary("places",);
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
 
     // Restrict within the map viewport.
-    let center = new google.maps.LatLng(52.369358, 4.889258);
+    // let center = new google.maps.LatLng(52.369358, 4.889258);
     const request = {
         // required parameters
         fields: ["displayName", "location", "businessStatus"],
         locationRestriction: {
-            center: center,  //기준점
+            center: map.getCenter(),  //기준점
             radius: 500,    //500 범위
         },
         // optional parameters
         includedPrimaryTypes: ["restaurant"], //음식점 
-        maxResultCount: 5,  //최대 5개
+        maxResultCount: 15,  //최대 5개
         rankPreference: SearchNearbyRankPreference.POPULARITY, //유명한거 기준
-        language: "en-US",  // 언어설정 
-        region: "us",  //지역
+        // language: "en-US",  // 언어설정 
+        // region: "us",  //지역
     };
     //@ts-ignore
     const { places } = await Place.searchNearby(request); //주변 검색 (request)를 기준으로
@@ -35,11 +38,12 @@ export async function nearbySearch(map,) {
 
         // Loop through and get all the results.
         places.forEach((place) => {   //각 장소마다 마커 찍어줄거임
-            const markerView = new AdvancedMarkerElement({
-                map,
-                position: place.location,
-                title: place.displayName,
-            });
+            // const markerView = new AdvancedMarkerElement({
+            // map,
+            // position: place.location,
+            // title: place.displayName,
+            // });
+            marker(map, place.location, infoWindow);
 
             bounds.extend(place.location);
             console.log(place);
