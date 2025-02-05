be205569ebe0ac0e902a4a4d642d9f264df124a9
HotNessie <fkdhswpsk7@gmail.com>
2025-02-05 16:31:56 +0900

    feat: google maps api AutoComplete Ver0.4.2

--- src/main/resources/static/js/map/initMap.js
+++ src/main/resources/static/js/map/initMap.js
@@ -1,24 +1,22 @@
 //지도 기본 위치
 //지도 기본 위치
 let map, infoWindow;
-let currentMarker = null;
 
 import { marker } from './marker.js';
 import { currentPosition } from './withPosition.js';
 import { findPlaces } from './findPlaces.js';
 import { nearbySearch } from './findNearPlaces.js';
-import { getPlaceDetails } from './getPlaceDetails.js';
 
 async function initMap() {
 
   const { Map } = await google.maps.importLibrary("maps");
   const { ColorScheme } = await google.maps.importLibrary("core");
-  const { PlaceAutocompleteElement } = await google.maps.importLibrary("places");
-  const { Place, AutocompleteSessionToken, AutocompleteSuggestion } =
+  const { Place, SearchNearbyRankPreference, AutocompleteSessionToken, AutocompleteSuggestion } =
     await google.maps.importLibrary("places");
 
   infoWindow = new google.maps.InfoWindow();
 
+  //이거 기본값 어디인거임...?
   const defaultPos = { lat: 37.65564466099954, lng: 127.06206796919646 };
 
   //Default(current) location  
@@ -47,24 +45,25 @@ async function initMap() {
         colorScheme: ColorScheme.DARK
       }
     );
-    marker(map, map.getCenter(), infoWindow);
+    marker(map, map.getCenter(), "NOW", infoWindow);
     return map;
   }
 
 
-  //---------------------AutoCompleteElement---------------------
-  //---------------------AutoCompleteElement---------------------
-  //---------------------AutoCompleteElement---------------------
-  const token = new google.maps.places.AutocompleteSessionToken();
+  //---------------------AutoComplete---------------------
+  //---------------------AutoComplete---------------------
+  //---------------------AutoComplete---------------------
+  const token = new AutocompleteSessionToken();
   const searchInput = document.getElementById("searchInput");
 
   searchInput.addEventListener("input", handleSearch);
-  searchInput.addEventListener("keyup", handleEmptyInput);
+
+  const suggestion = document.getElementById("suggestion");
 
   async function handleSearch() {
     const inputText = searchInput.value.trim();
     const resultsElement = document.getElementById("results");
-    const suggestion = document.getElementById("suggestion");
+    // const suggestion = document.getElementById("suggestion");
     if (inputText === "") {
       suggestion.style.display = "none";
       return;
@@ -78,7 +77,7 @@ async function initMap() {
       sessionToken: token
     };
 
-    const { suggestions } = await google.maps.places.AutocompleteSuggestion.fetchAutocompleteSuggestions(request);
+    const { suggestions } = await AutocompleteSuggestion.fetchAutocompleteSuggestions(request);
 
     // 기존 리스트 초기화
     resultsElement.innerHTML = "";
@@ -99,22 +98,48 @@ async function initMap() {
       resultsElement.appendChild(listItemButton);
       listItemButton.appendChild(listItem);
     }
-
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
     let selectedIndex = -1; // 현재 선택된 추천 검색어의 인덱스
 
-    searchInput.addEventListener("keydown", handleKeyDown);
+    //자동완성 텍스트 선택 이벤트
+    searchInput.addEventListener("keydown", selectSuggestion);
+    searchInput.addEventListener("keyup", handleEmptyInput);
 
-    function handleKeyDown(event) {
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
+    function selectSuggestion(event) {
       const items = document.querySelectorAll("#results button"); // 모든 추천 버튼 가져오기
 
       if (items.length === 0) return;
 
       if (event.key === "ArrowDown") {
         event.preventDefault();
-        selectedIndex = (selectedIndex + 1) % items.length; // 다음 항목 선택
+        if ((selectedIndex + 1) < items.length) {
+          selectedIndex = (selectedIndex + 1); // 다음 항목 선택
+        } else if (selectedIndex + 1 === items.length) {
+          selectedIndex = 0;
+        }
+        console.log(selectedIndex);
+        console.log();
+
+        // selectedIndex = (selectedIndex + 1) % items.length; // 다음 항목 선택
       } else if (event.key === "ArrowUp") {
         event.preventDefault();
-        selectedIndex = (selectedIndex - 1 + items.length) % items.length; // 이전 항목 선택
+        if ((selectedIndex - 1) === -2) {
+          selectedIndex = items.length - 1;
+        } else if (selectedIndex > -2 || selectedIndex < items.length) {
+          selectedIndex = (selectedIndex - 1); // 이전 항목 선택
+        }
+        console.log(selectedIndex);
+        console.log();
+
       } else if (event.key === "Enter" && selectedIndex >= 0) {
         event.preventDefault();
         // items[selectedIndex].click(); // 선택된 항목 클릭
@@ -123,7 +148,7 @@ async function initMap() {
         suggestion.style.display = "none";
         selectedIndex = -1; // 선택 초기화
       }
-
+      //focuse 효과
       items.forEach((item, index) => {
         if (index === selectedIndex) {
           item.style.backgroundColor = "rgb(36,36,36)";
@@ -132,10 +157,18 @@ async function initMap() {
         }
       });
     }
-
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
     // 클릭하면 검색창에 반영
     resultsElement.addEventListener("click", handleSelection);
-
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
     //추천 검색어 이벤트
     function handleSelection(event) {
       if (event.target.tagName === "LI") {
@@ -144,76 +177,87 @@ async function initMap() {
         selectedIndex = -1; // 선택 초기화
       }
     };
-
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
+    //---------------------------------------------------
     //TODO: 리스트 혹은 검색버튼 눌렀을 때, 세션 재발급 시켜줘야 함
   }
-
+  //---------------------------------------------------
+  //---------------------------------------------------
+  //---------------------------------------------------
+  //---------------------------------------------------
+  //---------------------------------------------------
+  //input이 비어있으면 display = none
   async function handleEmptyInput() {
     const resultsElement = document.getElementById("results");
     const suggestion = document.getElementById("suggestion");
     if (searchInput.value.trim() === "") {
-      suggestion.style.display = "none"; // 입력값이 비면 바로 숨김
+      suggestion.style.display = "none";
       resultsElement.innerHTML = "";
     }
   }
 
+  //searchButton을 통해 searchInput의 텍스트를 검색
+  const searchButton = document.getElementById('searchButton');
+  searchButton.addEventListener("click", findBySearch);
 
-  // const placeAutocomplete = new PlaceAutocompleteElement();
-  // placeAutocomplete.id = "place-autocomplete-input";
+  let markers = [];
+  async function findBySearch() {
+    const inputText = searchInput.value.trim(); // 검색어 가져오기
+    suggestion.style.display = "none";
 
-  // const card = document.getElementById("autocomplete");
-  // card.appendChild(placeAutocomplete);
+    if (inputText === "") {
+      alert("검색어를 입력해주세요.");
+      return;
+    }
+    //요청 정보
+    const request = {
+      textQuery: inputText, // 검색어
+      fields: ["displayName", "location", "businessStatus"], // 가져올 필드
+      maxResultCount: 20,
+      // useStrictTypeFiltering: true, //빡씬 제한 (includedType를 기준으로 제한하는거임)
+    };
 
-  // placeAutocomplete.addEventListener("gmp-placeselect", async ({ place }) => {
-  //   await place.fetchFields({
-  //     fields: ["displayName", "formattedAddress", "location"],
-  //   });
-  //   if (place.viewport) {
-  //     map.fitBounds(place.viewport);
-  //   } else {
-  //     map.setCenter(place.location);
-  //     map.setZoom(17);
-  //   }
-  //   let content =
-  //     `
-  //     <div id="infowindow-content">
-  //       <span id="place-displayname" class="title">${place.displayName}</span><br />
-  //       <span id="place-address">${place.formattedAddress}</span>
-  //     </div>
-  //   `;
+    const { places } = await Place.searchByText(request);
 
-  //   updateInfoWindow(content, place.location);
-
-  //   // 기존 마커 제거
-  //   if (currentMarker) {
-  //     currentMarker.setMap(null);
-  //   }
-
-  //   // 새로운 마커 추가
-  //   const newMarker = await marker(map, place.location, infoWindow);
-  //   if (newMarker) {
-  //     currentMarker = newMarker; // 현재 마커 업데이트
-  //   }
-  // });
+    clearMarkers();
+    //하나씩 마커 찍어주기
+    if (places.length) {
+      // console.log(places);
+      places.forEach(async (place) => {
+        console.log(place.displayName);
+        // console.log(place.position);
+        const newMarker = await marker(map, place.location, place.displayName, infoWindow);
+        markers.push(newMarker);
+      });
+    } else {
+      alert("검색 결과가 없습니다.");
+    }
 
 
-  // const searchButton = document.getElementById("searchFood");
-
-  // // 검색 버튼 클릭 이벤트
-  // searchButton.addEventListener("click", () => {
-  //   nearbySearch(map, infoWindow);
-  // });
-
+    //기존 마커 삭제
+    function clearMarkers() {
+      if (markers.length > 0) {
+        markers.forEach((marker) => { marker.setMap(null); })
+      }
+      markers = [];
+    }
+  }
   //---------------------------------------------------
   //---------------------------------------------------
   //---------------------------------------------------
+  //---------------------------------------------------
+  //---------------------------------------------------
+
   //커스텀 버튼 모음
   //현재위치 버튼
   const locationButton = document.querySelector(".customCurrentPosition");
   const customZoomInButton = document.querySelector(".customZoomIn");
   const customZoomOutButton = document.querySelector(".customZoomOut");
 
-  //현재위치 이동 이벤트 리스너
+  //현재위치
   locationButton.addEventListener("click", () => currentPosition(map, infoWindow));
 
   //zoomin
