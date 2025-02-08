2df037f70c5e05f91ddffc13a565f870d5906148
HotNessie <fkdhswpsk7@gmail.com>
2025-02-08 23:01:26 +0900

    feat: google maps api AutoComplete 검색 후 시점 이동 Ver0.5.1

---
+++ src/main/resources/static/js/map/dom-elements.js
@@ -0,0 +1,4 @@
+export const searchInput = document.getElementById("searchInput");
+export const resultsElement = document.getElementById("results");
+export const suggestion = document.getElementById("suggestion");
+export const searchButton = document.getElementById('searchButton');
